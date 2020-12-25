9ca08ab322556c19e1cde32aef3ed3ddd57b790b
Rakesh <rakeshwins232@gmail.com>
2020-12-25 14:31:24 +0530

    completed home page

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -6,14 +6,24 @@ class MobileMenu {
     }
 
     showNav() {
-        console.log('please show the navigation');
         this.mainNav.classList.toggle('site-header__nav--show');
         this.menuIcon.classList.toggle('site-header__menu-icon--close');
+    }
+
+    closeNav() {
+        this.mainNav.classList.remove('site-header__nav--show');
+        this.menuIcon.classList.remove('site-header__menu-icon--close');
     }
 
     events() {
         this.menuIcon.addEventListener('click', () => {
             this.showNav();
+        });
+        document.addEventListener('click', evt => {
+            //to check if nav menu is clicked or not
+            if(!(evt.target.matches('.site-header__menu-icon') || evt.target.matches('.site-header__nav') || evt.target.matches('.site-header__nav ul') || evt.target.matches('.site-header__nav li') || evt.target.matches('.site-header__nav a'))) {
+                this.closeNav();
+            }
         });
     }
 }
