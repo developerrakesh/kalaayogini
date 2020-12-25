4aa38b65a38ac73a6e5546f66ebbbe870c74810b
Rakesh <rakeshwins232@gmail.com>
2020-12-26 03:31:08 +0530

    improving responsiveness of modal

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,7 +1,11 @@
+import debounce from 'lodash/debounce';
+
 class MobileMenu {
     constructor() {
         this.menuIcon = document.querySelector('.site-header__menu-icon');
         this.mainNav = document.querySelector('.site-header__nav');
+        this.header = document.querySelector('.site-header');
+        this.browserHeight = window.innerHeight;
         this.events();
     }
 
@@ -15,16 +19,29 @@ class MobileMenu {
         this.menuIcon.classList.remove('site-header__menu-icon--close');
     }
 
+    changeNavPosition() {
+        if(this.browserHeight < 400) {
+            this.header.style.position = 'absolute';
+        } else {
+            this.header.style.position = 'fixed';
+        }
+    }
+
     events() {
         this.menuIcon.addEventListener('click', () => {
             this.showNav();
         });
+        this.changeNavPosition();
         document.addEventListener('click', evt => {
             //to check if nav menu is clicked or not
             if(!(evt.target.matches('.site-header__menu-icon') || evt.target.matches('.site-header__nav') || evt.target.matches('.site-header__nav ul') || evt.target.matches('.site-header__nav li') || evt.target.matches('.site-header__nav a') || evt.target.matches('.site-header__nav__toggle-btn'))) {
                 this.closeNav();
             }
         });
+        window.addEventListener('resize', debounce(() => {
+            this.browserHeight = window.innerHeight;
+            this.changeNavPosition();
+        }, 300));
     }
 }
 
--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -1,29 +1,60 @@
+import debounce from 'lodash/debounce';
+
 class Modal {
     constructor() {
         this.insertModal();
+        this.browserHeight = window.innerHeight;
+        this.browserWidth = window.innerWidth;
     }
 
     insertModal() {
         document.body.insertAdjacentHTML('beforeend', `
         <div class="modal wrapper wrapper--center-children" role="region" aria-modal="true">
             <form class="modal__form">
-                <div class="modal__close" role="button" aria-label="close modal button"><img src="assets/images/svgs/x.svg" alt="close"></div>
+                <div class="modal__close" role="button" aria-label="close modal button"><img src="assets/images/svgs/x.svg" width="16" height="16" alt="close"></div>
                 <h2 class="modal__title">Contact Us</h2>
                 <p class="modal__msg">Please leave us a message and we will reach out to you.</p>
                 <fieldset class="modal__user-input">
-                    <input type="text" name="name" id="name" required placeholder="Name">
-                    <input type="email" name="email" id="email" required placeholder="Email">
-                    <input type="tel" name="phone" id="phone" placeholder="Phone No.">
-                    <textarea name="msg" id="msg" cols="30" rows="7" placeholder="Message"></textarea>
+                    <input type="text" name="name" id="name" required placeholder="Name" aria-label="name">
+                    <input type="email" name="email" id="email" required placeholder="Email" aria-label="email">
+                    <input type="tel" name="phone" id="phone" placeholder="Phone No." aria-label="phone number">
+                    <textarea name="msg" id="msg" cols="30" rows="7" placeholder="Message" aria-label="message"></textarea>
                 </fieldset>
                 <button class="btn modal__btn submit-btn">Submit</button>
             </form>
         </div>
+        <div class="modal-overlay"></div>
         `);
+    }
+
+    changeModalPosition() {
+        if(this.browserWidth < 678) {
+            if(this.browserHeight < 480) {
+                document.querySelector('.modal').style.position = 'absolute';
+                document.querySelector('.modal__form').style.alignSelf = 'flex-start';
+                document.querySelector('.modal__form').style.marginTop = '10px';
+            } else {
+                document.querySelector('.modal').style.position = 'fixed';
+                document.querySelector('.modal__form').style.alignSelf = 'center';
+                document.querySelector('.modal__form').style.marginTop = '0';
+            }
+        } else {
+            if(this.browserHeight < 556) {
+                document.querySelector('.modal').style.position = 'absolute';
+                document.querySelector('.modal__form').style.alignSelf = 'flex-start';
+                document.querySelector('.modal__form').style.marginTop = '10px';
+            } else {
+                document.querySelector('.modal').style.position = 'fixed';
+                document.querySelector('.modal__form').style.alignSelf = 'center';
+                document.querySelector('.modal__form').style.marginTop = '0';
+            }
+        }
     }
 
     openModal() {
         document.querySelector('.modal').classList.add('modal--show');
+        document.querySelector('.modal-overlay').classList.add('modal-overlay--show');
+        this.changeModalPosition();
         document.querySelector('.modal__close').addEventListener('click', () => {
             this.closeModal();
         });
@@ -32,10 +63,16 @@ class Modal {
                 this.closeModal();
             }
         });
+        window.addEventListener('resize', debounce(() => {
+            this.browserHeight = window.innerHeight;
+            this.browserWidth = window.innerWidth;
+            this.changeModalPosition();
+        }, 300));
     }
 
     closeModal() {
         document.querySelector('.modal').classList.remove('modal--show');
+        document.querySelector('.modal-overlay').classList.remove('modal-overlay--show');
     }
 }
 
